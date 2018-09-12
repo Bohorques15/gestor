d5c90e5cb640acd99b63a0a7f77fbda008347f0f
Orlando Luna <luna.orlando10@gmail.com>
2018-09-13 01:03:02 +0200

    Mostrar todas las noticias y categorias en la home

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -8,24 +8,45 @@ import { UsuarioService, NoticiaService } from '../servicios';
 export class HomeComponent implements OnInit {
     currentUser: Usuario;
     usuarios: Usuario[] = [];
+    noticias: Noticia[] = [];
+    categorias: any[] = [];
 
-    constructor(private usuario_servicio: UsuarioService) {
+    constructor(private usuario_servicio: UsuarioService, private noticia_service: NoticiaService) {
         this.currentUser = JSON.parse(localStorage.getItem('usuario'));
     }
 
     ngOnInit() {
-        //this.loadAllUsers();
+        this.cargar_noticias();
     }
 
     /*deleteUser(id: number) {
         this.userService.delete(id).pipe(first()).subscribe(() => { 
             this.loadAllUsers() 
         });
+    }*/
+
+    private cargar_noticias() {
+        this.noticia_service.obtenerTodas().pipe(first()).subscribe(noticias => { 
+            this.noticias = noticias;
+            this.categorias = this.noticias.map(function (noticia) {
+                return noticia.clasificacion;
+            });
+            //this.categorias = this.cargar_categorias(this.categorias);
+        });
     }
 
-    private loadAllUsers() {
-        this.userService.getAll().pipe(first()).subscribe(users => { 
-            this.users = users; 
+    /*private cargar_categorias(categorias){
+        Array.prototype.unique = function(a){
+           return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
         });
+        return categorias.unique();
     }*/
+
+    filtrarCategoria(categoria){
+        this.noticia_service.obtenerPorCategoria(categoria).pipe(first()).subscribe(noticias => { 
+            this.noticias = noticias;
+        });
+    }
+
+
 }
--- src/app/Servicios/noticia.service.ts
+++ src/app/Servicios/noticia.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Noticia } from '../modelos';
+
+
 
 @Injectable()
 export class NoticiaService {
 	constructor(private http: HttpClient) { }
+
+	obtenerTodas() {
+        return this.http.get<Noticia[]>(`http://localhost:8000/api/noticias`);
+    }
+
+    obtenerPorCategoria(categoria: string) {
+        return this.http.get<Noticia[]>(`http://localhost:8000/api/noticias/clasificacion` + categoria);
+    }
+
+
 }
